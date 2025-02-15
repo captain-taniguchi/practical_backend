668a1ae2c91008839d0137738d839457f57811c6
captain-taniguchi <taniyasu7@gmail.com>
2025-02-15 16:27:09 +0900

    リモートレポジトリを作って、初プッシュしました

--- db_control/connect_MySQL.py
+++ db_control/connect_MySQL.py
@@ -3,8 +3,8 @@ from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv #.envファイルを読み込むためのライブラリ
 
-# 環境変数の読み込み
-load_dotenv()
+# Azure環境でなければ .env を読み込む ★要確認
+if not os.getenv("AZURE_ENVIRONMENT"): load_dotenv()
 
 # データベース接続情報
 DB_USER = os.getenv('DB_USER')
@@ -20,12 +20,12 @@ print("DB_HOST:", os.getenv("DB_HOST"))
 print("DB_PORT:", os.getenv("DB_PORT"))
 print("DB_NAME:", os.getenv("DB_NAME"))
 
-# SSL証明書のパス
+# SSL証明書のパス(★この記述だけだと通らかなった)
 ssl_cert=str('DigiCertGlobalRootCA.crt.pem')
 print("ssl_cert:", ssl_cert)
 
 # MySQLのURL構築
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?ssl_ca=DigiCertGlobalRootCA.crt.pem&ssl_verify_cert=true" #0208SSL追加
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?ssl_ca=DigiCertGlobalRootCA.crt.pem&ssl_verify_cert=true" #0208SSL追加★
 
 # エンジンの作成
 engine = create_engine(
